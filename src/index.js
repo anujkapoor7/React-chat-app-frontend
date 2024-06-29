b66020f2fae485c9ced7443a6b5f4485e3e0be00
Anuj Kapoor <anuj1999kapoor@gmail.com>
2024-06-29 15:04:51 +0530

    ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+import "./global.scss";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
